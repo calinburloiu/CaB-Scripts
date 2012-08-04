aae3b8a6213f4ae196e59ee7bd4751f2a45a4ce9
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2012-08-04 10:29:28 +0800

    added two find scripts written during NUS internship

---
+++ Programming/find-in-jars.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Author: Calin-Andrei Burloiu
+#
+
+if [ $# -ne 2 ]; then
+	echo "usage: $0 HAYSTACK_PATH NEEDLE" >&2
+	echo -e "Grep through the list of files packed into JARs from\n"\
+"HAYSTACK_PATH." >&2
+	exit 1
+fi
+
+HAYSTACK="$1"
+NEEDLE="$2"
+
+IFS=$'\n'
+
+for i in $(find $HAYSTACK -name "*.jar"); do
+	if [[ $(jar -tf "$i" | grep "$2") ]]; then
+		echo "$i"
+	fi
+done
