d724577fc13c05eccaf2e334f7a6fe737b47721f
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2014-02-06 11:37:39 +0200

    util: Solve arguments bug in file size statistics scritps.

--- util/file-size-statistics.sh
+++ util/file-size-statistics.sh
@@ -3,7 +3,7 @@
 # Author: Calin-Andrei Burloiu
 #
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 1 ]; then
 	echo "Usage: $0 DIRECTORY [SEARCH_PATTERN]" >&2
 	echo -e "Print file size statistics for files from DIRECTORY filtering results\n"\
 "by the optional SEARCH_PATTERN." >&2
