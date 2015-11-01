e71c2dd8ddb5228ebef1d44aa6183610b2f2d2c8
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2015-11-01 18:03:35 +0200

    Fix license scripts.

--- util/prepend_license/prepend-license.sh
+++ util/prepend_license/prepend-license.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 USAGE=$"$(cat <<EOF
-Usage: $0 <license_header_file> <skip_pattern> <target_dir> <source_extension>
+Usage: $0 <license_header_file> <skip_pattern> <target_dir> <source_find_pattern>
 
-Add the content of the <license_header_file> to all source files which has the
-extension <source_extension>. <skip_pattern> should be a pattern used by grep
-to identify if the source file already has the license header in order to be
-skipped.
+Add the content of the <license_header_file> to all source files which match
+find command pattern <source_find_pattern>. <skip_pattern> should be a pattern
+used by grep to identify if the source file already has the license header in
+order to be skipped.
 EOF
 )"
 
@@ -17,10 +17,10 @@ fi
 HEADER="$1"
 SKIP_PATTERN="$2"
 TARGET="$3"
-EXT=$4
+PATTERN=$4
 
 IFS=$'\n'
-for src in $(find "$TARGET" -name "*.${EXT}"); do
+for src in $(find "$TARGET" -name "${PATTERN}"); do
     if [ -z "$(grep "$SKIP_PATTERN" "$src")" ]; then
         cat "$HEADER" "$src" > "${src}.tmp"
         mv "${src}.tmp" "$src"
